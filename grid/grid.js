37e376239c6ea82aff07ce65a8ad0f511c2fd736
Tyler Tepper <157242593+ttepper@users.noreply.github.com>
2025-04-07 21:25:09 -0500

    Add files via upload

---
+++ grid/grid.js
@@ -0,0 +1,28 @@
+function change01() 
+{
+    document.getElementById("large").src = "images/cocobed-gimp.JPG";
+}
+  
+function change02() 
+{
+    document.getElementById("large").src = "images/cococouch-gimp.JPG";
+}
+
+function change03() 
+{
+    document.getElementById("large").src = "images/cocofloor-gimp.jpg";
+}
+  
+function change04() 
+{
+    document.getElementById("large").src = "images/cocograss-gimp.jpg";
+}
+  
+function change05() 
+{
+    document.getElementById("large").src = "images/fire-gimp.JPG";
+}
+function change06() 
+{
+    document.getElementById("large").src = "images/firerotate-gimp.JPG";
+}
