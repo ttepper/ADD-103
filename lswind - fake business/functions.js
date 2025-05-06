39a44e99a1cf5db3da140f75401889d74da7bcb2
Tyler Tepper <157242593+ttepper@users.noreply.github.com>
2025-05-05 20:15:07 -0500

    Delete lswind - fake business directory

--- lswind - fake business/functions.js
+++
@@ -1,6 +0,0 @@
-// Bootstarp 5 Popover w3schools
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
-  
