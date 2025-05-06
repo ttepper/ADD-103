146e03f78d4f21dc3cc7addfae315c4614ba668e
Tyler Tepper <157242593+ttepper@users.noreply.github.com>
2025-05-05 20:10:08 -0500

    Add files via upload

---
+++ lswind - fake business/functions.js
@@ -0,0 +1,6 @@
+// Bootstarp 5 Popover w3schools
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+  
